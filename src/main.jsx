0da7137df6f7dcbf5bf05d31a2064e54d85ad0c0
AndresCRomo <andres.calderon@miaa.mx>
2024-08-08 15:43:01 -0600

    Agrego Modelos de discos de vynil al proyecto y hacemos pruevas de posicionamiento luz y texturas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,117 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import gsap from "gsap";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setClearColor(0xffff00, 1);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(5, 5, 11);
+camera.lookAt(0, 0, 0);
+
+const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
+groundGeometry.rotateX(-Math.PI / 2);
+const groundMaterial = new THREE.MeshBasicMaterial({
+  color: 0x555555,
+  side: THREE.DoubleSide,
+});
+const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+ground.position.y = -10;
+scene.add(ground);
+
+const ambientlight = new THREE.AmbientLight(0xffffff, 1);
+ambientlight.position.set(0, 70, 0);
+scene.add(ambientlight);
+
+const light = new THREE.SpotLight(0xffffff, 1, 70, 0.7, 0);
+light.position.set(0, 30, 0);
+scene.add(light);
+
+const loader = new GLTFLoader();
+const textureLoader = new THREE.TextureLoader();
+const normalTexture = textureLoader.load(
+  "src/assets/3dmodels/12_vinyl_record/textures/REC0002_Textures_normal.png"
+);
+const metalTexture = textureLoader.load(
+  "src/assets/3dmodels/12_vinyl_record/textures/REC0002_Textures_metallicRoughness.png"
+);
+const baseTexture = textureLoader.load(
+  "src/assets/3dmodels/12_vinyl_record/textures/REC0002_Textures_baseColor.png"
+);
+
+baseTexture.wrapS = THREE.RepeatWrapping;
+baseTexture.wrapT = THREE.RepeatWrapping;
+baseTexture.rotation = Math.PI / 2; // Rota la textura 90 grados
+
+let disc;
+
+loader.load(
+  "src/assets/3dmodels/12_vinyl_record.glb",
+  function (gltf) {
+    disc = gltf.scene;
+
+    disc.scale.set(50, 50, 50);
+    disc.rotation.set(0, 1.2, Math.PI / 2);
+
+    /*disc.traverse(node=>{
+    if(node.isMesh){
+      node.material.map=baseTexture;
+      node.material.normalMap=normalTexture;
+      node.material.roughnessMap= metalTexture;
+      node.material.metalnessMap= metalTexture;
+      
+    } 
+  })*/
+    scene.add(disc);
+    animate();
+    animateDisc();
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.enableZoom = true;
+controls.enablePan = true;
+controls.minDistance = 5;
+controls.maxDistance = 20;
+controls.minPolarAngle = 0.5;
+controls.maxPolarAngle = 1.5;
+controls.autoRotate = false;
+controls.target = new THREE.Vector3(0, 0, 0);
+controls.update();
+
+function animateDisc() {
+  gsap.to(disc.rotation, {
+    duration: 4,
+    z: Math.PI * 2,
+    repeat: -1,
+    ease: "none",
+  });
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
+createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
--- src/models/Carrusel.jsx
+++ src/models/Carrusel.jsx
@@ -1,32 +1,124 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { useRef } from 'react';
+import { useRef,useState } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 
 gsap.registerPlugin(useGSAP);
 
 
 function Carrusel() {
     
-    const container = useRef();
+
     
 
-    const rotateDisc =
+    const disco = useRef();
+    const [toggle, setToggle] = useState(0);
+
     useGSAP(
         () => {
             
-            gsap.to('.disco', {rotation:45})
+            gsap.to(disco.current, {rotate: toggle, duration: .5, ease: 'none'});
              // <-- automatically reverted
         },
-        { scope: container }
-    ); // <-- scope
+        
+    [toggle]); // <-- scope
+
+        
     return (
         <div className="w-full overflow-clip relative flex justify-center items-center h-[400px] border-2 border-purple-300">
-            <section ref={container}>
-                <button onClick={ rotateDisc } className='rounded-full bg-slate-500 font-medium text-white'>toggle</button>
-                <img  className=' disco absolute top-1/2 -left-[230px] size-[450px] object-cover'  src="src\assets\images\disco.png" alt="" />
-            </section>
+            
+                <button onClick={()=> setToggle(toggle + 45)}  className='rounded-full bg-slate-500 font-medium text-white'>toggle</button>
+                <div ref={disco} className=' disco absolute   left-[230px] size-[420px]  rounded-full flex items-center justify-center  border-[2px]'
+                style={{
+                    background: "radial-gradient(circle, #0c0c0c ,#2e2e2e )",
+                }}
+                alt="">
+                    <div className='absolute '>
+                        <div className=' size-[380px] ' 
+                        style={{
+                            border: "3px solid transparent",
+                            borderTopColor:"#3b3b3b",
+                            borderRadius: "80%",
+                            transform: "rotate(-205deg)",
+                        }}
+                        ></div>
+                    </div>
+                    <div className='absolute '>
+                        <div className=' size-[340px] ' 
+                        style={{
+                            border: "3px solid transparent",
+                            borderTopColor:"#3b3b3b",
+                            borderRadius: "80%",
+                            transform: "rotate(105deg)",
+                        }}
+                        ></div>
+                    </div>
+                    <div className='absolute '>
+                        <div className=' size-[360px] ' 
+                        style={{
+                            border: "3px solid transparent",
+                            borderTopColor:"#3b3b3b",
+                            borderRadius: "80%",
+                            transform: "rotate(85deg)",
+                        }}
+                        ></div>
+                    </div>
+                    <div className='absolute '>
+                        <div className=' size-[375px] ' 
+                        style={{
+                            border: "3px solid transparent",
+                            borderTopColor:"#3b3b3b",
+                            borderRadius: "80%",
+                            transform: "rotate(-28deg)",
+                        }}
+                        ></div>
+                    </div>
+                    <div className='absolute '>
+                        <div className=' size-[345px] ' 
+                        style={{
+                            border: "3px solid transparent",
+                            borderTopColor:"#3b3b3b",
+                            borderRadius: "80%",
+                            transform: "rotate(-68deg)",
+                        }}
+                        ></div>
+                    </div>
+                    <div className='absolute '>
+                        <div className=' size-[325px] ' 
+                        style={{
+                            border: "3px solid transparent",
+                            borderTopColor:"#3b3b3b",
+                            borderRadius: "80%",
+                            transform: "rotate(-98deg)",
+                        }}
+                        ></div>
+                    </div>
+                    <div className='absolute '>
+                        <div className=' size-[275px] ' 
+                        style={{
+                            border: "3px solid transparent",
+                            borderTopColor:"#3b3b3b",
+                            borderRadius: "1000%",
+                            transform: "rotate(28deg)",
+                        }}
+                        ></div>
+                    </div>
+                    <div className='absolute '>
+                        <div className=' size-[295px] ' 
+                        style={{
+                            border: "3px solid transparent",
+                            borderTopColor:"#3b3b3b",
+                            borderRadius: "1000%",
+                            transform: "rotate(128deg)",
+                        }}
+                        ></div>
+                    </div>
+                <div className=' bg-[#d4f361] size-[100px] rounded-full text-white font-bold text-2xl text-center flex justify-center items-center'> MCLOVIN ROLLS</div>
+                </div>
         </div>
     )
 }
