cad2328d73205b81566dfc1582b7590ce95310f8
unknown <jeduardo.martinez@miaa.mx>
2024-08-29 06:50:14 -0600

    formulario de reserva tu clase

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { IoLocationSharp } from "react-icons/io5";
 import { FaClock } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
@@ -20,9 +19,9 @@ export const ContactUs = () => {
                 width="600"
                 height="450"
                 style={{ border: "0", margin: "auto" }}
-                allowfullscreen=""
+                allowFullScreen=""
                 loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"
+                referrerPolicy="no-referrer-when-downgrade"
               ></iframe>
             </div>
             <div>
@@ -81,22 +80,22 @@ export const ContactUs = () => {
             Contactanos
           </h1>
         </div>
-        <section class="bg-[#0e0e0e] relative">
-          <div class="absolute inset-0 bg-[#0e0e0e]">
+        <section className="bg-[#0e0e0e] relative">
+          <div className="absolute inset-0 bg-[#0e0e0e]">
             <iframe
               width="98%"
               height="100%"
-              frameborder="0"
-              marginheight="0"
-              marginwidth="0"
+              frameBorder="0"
+              marginHeight="0"
+              marginWidth="0"
               title="map"
               scrolling="no"
               style={{margin: "auto"}}
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3702.6603474446742!2d-102.3137554249437!3d21.87063317999779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8429ef9ed36aabd1%3A0x45990aec815771e7!2sEstudio%20de%20danza%205%2C%206%2C%207%2C%208!5e0!3m2!1sen!2smx!4v1724107193091!5m2!1sen!2smx"
             ></iframe>
           </div>
-          <div class="container px-5 py-24 mx-auto flex">
-            <div class="lg:w-[26%] md:w-1/3 bg-[#ffffffeb] rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+          <div className="container px-5 py-24 mx-auto flex">
+            <div className="lg:w-[26%] md:w-1/3 bg-[#ffffffeb] rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
             
             <div>
               <div className="w-full mx-auto rounded-lg overflow-hidden">
