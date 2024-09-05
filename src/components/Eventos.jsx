d479636a24e84683c3a30d7e7d9c4ce703c7a78e
unknown <quitze.perez@ccapama.gob.mx>
2024-09-04 23:01:32 -0600

    Evetos

--- src/components/Eventos.jsx
+++ src/components/Eventos.jsx
@@ -3,58 +3,41 @@ import eventomes from "../../src/assets/images/Evento/Eventomes.png"
 
 const Eventos = () => {
   return (
-   <div className="relative h-screen">
-  <div className="absolute inset-0">
-    {/* Gradient Overlay */}
-    <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black  z-20"></div>
-    <img
-      src={fondoladrillo}
-      alt="delante"
-      className="absolute inset-0 object-cover w-full h-full"
-    />
-  </div>
-
-      {/*usar flex para cambiar el manejo de los elementos texto /letras */ }
-  <div className="absolute mt-[250px] inset-0 flex justify-center">
-    <div>
-      <img src={eventomes} className="w-[360px] h-[450px]" />
-    </div>
-      </div>
-          <div className="   w-full flex items-center flex-col  ">
-      
-        <div className=" rounded-t-xl h-[240px]  justify-center  z-10 flex flex-col">
-          <div className="flex flex-row ">
-            
-            <p
-              id="hero"
-              className="  leading-none font-raleway text-white text-[40px] md:text-[60px] lg:text-[80px] xl:text-[120px] font-extrabold opacity-0 "
-            >
-              Aprende a{" "}
-            </p>
-            <p
-              id="hero"
-              className="  leading-none font-raleway text-white text-[40px] md:text-[60px] lg:text-[80px] xl:text-[120px] decoration-primary underline-offset-2  underline  font-extrabold opacity-0 ml-3"
-            >
-              Bailar{" "}
-            </p>
-            <p
-              id="hero"
-              className="  leading-none font-raleway text-white text-[40px] md:text-[60px] lg:text-[80px] xl:text-[120px] font-extrabold opacity-0 "
-            >
-              ,
-            </p>
-          </div>
-          <p
-            id="hero"
-            className=" leading-none font-raleway text-white text-[40px] md:text-[60px] lg:text-[80px] xl:text-[120px] font-extrabold opacity-0 "
-          >
-            Con Estilo
+    <section className=" text-white py-20 px-4">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <div className="md:w-1/2 mb-10 md:mb-0">
+          <h2 className="text-4xl font-bold leading-tight mb-4  ">
+            Nuevos eventos y talleres cada Mes.
+          </h2>
+          <p className="text-xl mb-4 bg-clip-text text-transparent bg-gradient-to-r from-yellow-600 to-yellow-50">
+            Acercate, Pregunta y ¡Animate a inscribirte!
           </p>
-    
+
+          <div className=" cursor-pointer">
+            <button
+              onClick={() => { }}
+              className="rounded-lg md:text-sm hover:bg-primary whitespace-nowrap md:px-2  hover:font-bold hover:text-secondary  border-2 transition-all ease-in-out  border-primary text-white px-5 py-2 font-raleway"
+            >
+             Informes Porfavor 😁🙏
+            </button>
+          </div>
+        </div>
+        <div className="relative md:w-1/2">
+          <img
+            src={fondoladrillo}
+            alt="Hero Image"
+            className="w-full rounded-xl "
+          />
+          <div className="absolute inset-0 flex justify-center items-center ">
+            <img
+              src={eventomes}
+              className=" relative h-auto max-w-72  rounded-lg"
+            />
+          </div>
         </div>
       </div>
-</div>
-  )
+    </section>
+  );
 }
 
 export default Eventos
